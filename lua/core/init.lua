c2d8c406a6dc5e260a5d4645cca6af3ec08e8a99
Pcrab <pcrab@pcrab.xyz>
2022-11-18 18:09:28 +0800

    init repo

---
+++ lua/core/init.lua
@@ -0,0 +1,41 @@
+-- basics
+vim.cmd("syntax on")
+vim.cmd("filetype plugin indent on")
+vim.opt.number         = true
+vim.opt.relativenumber = true
+vim.opt.termguicolors  = true
+vim.opt.shiftround     = true
+vim.opt.updatetime     = 100
+vim.opt.cursorline     = true
+vim.opt.autowrite      = true
+if (vim.fn.has("termguicolors") == 1) then
+	vim.opt.termguicolors = true
+end
+-- tabs
+vim.opt.autoindent  = true
+vim.opt.tabstop     = 4
+vim.opt.shiftwidth  = 4
+vim.opt.softtabstop = 4
+vim.opt.mouse       = "a"
+vim.opt.expandtab   = true
+vim.opt.autowrite   = false
+vim.opt.formatoptions = ""
+
+require("core.plugins")
+
+vim.g.loaded_matchparen        = 1
+vim.g.loaded_matchit           = 1
+vim.g.loaded_logiPat           = 1
+vim.g.loaded_rrhelper          = 1
+vim.g.loaded_tarPlugin         = 1
+vim.g.loaded_gzip              = 1
+vim.g.loaded_zipPlugin         = 1
+vim.g.loaded_2html_plugin      = 1
+vim.g.loaded_shada_plugin      = 1
+vim.g.loaded_spellfile_plugin  = 1
+vim.g.loaded_netrw             = 1
+vim.g.loaded_netrwPlugin       = 1
+vim.g.loaded_tutor_mode_plugin = 1
+vim.g.loaded_remote_plugins    = 1
+
+require("config")
---
+++ lua/core/plugins.lua
@@ -0,0 +1,100 @@
+local ensure_packer = function()
+    local fn = vim.fn
+    local install_path = fn.stdpath("data").."/site/pack/packer/start/packer.nvim"
+    if fn.empty(fn.glob(install_path)) > 0 then
+        fn.system({"git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path})
+        vim.cmd [[packadd packer.nvim]]
+        return true
+    end
+    return false
+end
+
+local packer_bootstrap = ensure_packer()
+
+return require("packer").startup(function(use)
+    use "wbthomason/packer.nvim"
+
+    -- Begin define custom plugins
+
+    -- Optimize startup time
+    use {
+        "dstein64/vim-startuptime",
+        "lewis6991/impatient.nvim",
+        "nathom/filetype.nvim"
+    }
+
+    --
+    -- Operations
+    --
+
+    -- buffer
+    use {
+		"akinsho/bufferline.nvim",
+		requires = "kyazdani42/nvim-web-devicons"
+	}
+	use "moll/vim-bbye"
+    -- file
+    use {
+		"kyazdani42/nvim-tree.lua",
+		requires = "kyazdani42/nvim-web-devicons"
+	}
+    -- git
+    use {
+		"lewis6991/gitsigns.nvim",
+		tag = "release",
+	}
+
+    --
+    -- Language
+    --
+
+    -- LSP
+    use {
+        "williamboman/mason.nvim",
+        "williamboman/mason-lspconfig.nvim",
+        "neovim/nvim-lspconfig",
+    }
+    -- CMP
+    use {
+        "hrsh7th/cmp-nvim-lsp",
+        "hrsh7th/cmp-buffer",
+        "hrsh7th/cmp-path",
+        "hrsh7th/cmp-cmdline",
+        "hrsh7th/nvim-cmp",
+
+        "L3MON4D3/LuaSnip",
+        "saadparwaiz1/cmp_luasnip",
+    }
+    -- Specific Language
+    use {
+        "simrat39/rust-tools.nvim"
+    }
+
+    --
+    -- GUI
+    --
+    use {
+        "EdenEast/nightfox.nvim",
+        config = function ()
+            vim.cmd("colorscheme nightfox")
+        end
+    }
+    use {
+        "goolord/alpha-nvim",
+        config = function ()
+            require("alpha").setup(require"alpha.themes.dashboard".config)
+        end
+    }
+    use {
+		"nvim-lualine/lualine.nvim",
+		requires = "kyazdani42/nvim-web-devicons"
+	}
+
+    -- End of custom plugins
+
+    -- Automatically set up your configuration after cloning packer.nvim
+    -- Put this at the end after all plugins
+    if packer_bootstrap then
+        require("packer").sync()
+    end
+end)
