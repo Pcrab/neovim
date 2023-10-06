5ff110a81fa334ff3054d3e190da08ba0f92301c
pcrab <pcrab@pcrab.xyz>
2023-10-06 13:21:24 +0800

    feat: update plugins

--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -1,27 +1,27 @@
 local highlight = {
-	"RainbowDelimiterRed",
-	"RainbowDelimiterYellow",
-	"RainbowDelimiterBlue",
-	"RainbowDelimiterOrange",
-	"RainbowDelimiterGreen",
-	"RainbowDelimiterViolet",
-	"RainbowDelimiterCyan",
+    "RainbowDelimiterRed",
+    "RainbowDelimiterYellow",
+    "RainbowDelimiterBlue",
+    "RainbowDelimiterOrange",
+    "RainbowDelimiterGreen",
+    "RainbowDelimiterViolet",
+    "RainbowDelimiterCyan",
 }
 
 return {
-	"lukas-reineke/indent-blankline.nvim",
-	event = "VeryLazy",
-	branch = "v3",
-	opts = {
-		scope = {
-			enabled = true,
-			highlight = highlight,
-		},
-	},
-	config = function(_, opts)
-		require("ibl").setup(opts)
+    "lukas-reineke/indent-blankline.nvim",
+    event = "VeryLazy",
+    main = "ibl",
+    opts = {
+        scope = {
+            enabled = true,
+            highlight = highlight,
+        },
+    },
+    config = function(_, opts)
+        require("ibl").setup(opts)
 
-		local hooks = require("ibl.hooks")
-		hooks.register(hooks.type.SCOPE_HIGHLIGHT, hooks.builtin.scope_highlight_from_extmark)
-	end,
+        local hooks = require("ibl.hooks")
+        hooks.register(hooks.type.SCOPE_HIGHLIGHT, hooks.builtin.scope_highlight_from_extmark)
+    end,
 }
