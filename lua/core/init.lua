6b3f9fd5c7022706d80e112961887cb07337de54
pcrab <pcrab@pcrab.xyz>
2023-10-28 19:39:06 +0800

    feat: change colorscheme

--- lua/core/init.lua
+++ lua/core/init.lua
@@ -3,7 +3,7 @@ require("core.lazy")
 require("core.remap")
 require("core.lsp")
 
-vim.cmd.colorscheme("catppuccin")
+vim.cmd.colorscheme("onedark")
 
 vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,41 +1,4 @@
 return {
-	"catppuccin/nvim",
-	name = "catppuccin",
-	lazy = true,
-	opts = {
-		term_colors = true,
-		-- custom_highlights = function(color)
-		-- 	return {
-		-- 		TabLine = { bg = color.surface0, fg = color.subtext0 },
-		-- 		TabLineFill = { fg = color.subtext0, bg = color.mantle },
-		-- 		TabLineSel = { fg = color.base, bg = color.overlay1 },
-		--
-		-- 		MatchParen = { fg = "NONE", bg = color.surface1, style = { "bold" } },
-		-- 	}
-		-- end,
-		integrations = {
-			aerial = true,
-			fidget = true,
-			markdown = true,
-			mason = true,
-			neotree = true,
-			native_lsp = {
-				underlines = {
-					errors = { "undercurl" },
-					hints = { "undercurl" },
-					warnings = { "undercurl" },
-					information = { "undercurl" },
-				},
-			},
-			navic = {
-				enabled = true,
-			},
-			treesitter_context = true,
-			octo = true,
-			overseer = true,
-			symbols_outline = true,
-			illuminate = true,
-			which_key = true,
-		},
-	},
+    "olimorris/onedarkpro.nvim",
+    priority = 1000,
 }
