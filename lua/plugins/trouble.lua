608bd8bad857ff505a7bcd5179099798fd5196e5
pcrab <pcrab@pcrab.xyz>
2023-10-29 17:27:50 +0800

    feat: todo comments

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -1,25 +1,46 @@
 return {
     "folke/trouble.nvim",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
+    dependencies = {
+        "nvim-tree/nvim-web-devicons",
+        {
+            "folke/todo-comments.nvim",
+            dependencies = { "nvim-lua/plenary.nvim" },
+            opts = {},
+        },
+    },
+    lazy = false,
     opts = {
-        -- your configuration comes here
-        -- or leave it empty to use the default settings
-        -- refer to the configuration section below
+        auto_close = true,
     },
     keys = {
         {
             "<leader>xw",
             mode = { "n" },
             function()
-                require("trouble").toggle("workspace_diagnostics")
+                require("trouble").open("workspace_diagnostics")
             end,
+            desc = "Workspace Diagnostics",
         },
         {
             "<leader>xd",
             mode = { "n" },
             function()
-                require("trouble").toggle("document_diagnostics")
+                require("trouble").open("document_diagnostics")
             end,
+            desc = "Document Diagnostics",
+        },
+        {
+            "<leader>xt",
+            mode = { "n" },
+            "<cmd>TodoTrouble<cr>",
+        },
+        {
+            "gR",
+            mode = { "n" },
+            function()
+                require("trouble").open("lsp_references")
+            end,
+            desc = "LSP References",
         },
     },
 }
