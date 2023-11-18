4235ba3d4d1fa5a5b95f47a3c09615d6cb9aa3ed
pcrab <pcrab@pcrab.xyz>
2023-11-18 13:12:10 +0800

    feat: tabline

--- lua/plugins/tabline.lua
+++ lua/plugins/tabline.lua
@@ -1,32 +1,69 @@
 return {
     {
-        "akinsho/bufferline.nvim",
-        version = "*",
-        dependencies = "nvim-tree/nvim-web-devicons",
+        "nanozuki/tabby.nvim",
         lazy = false,
+        config = function()
+            local values = require("core.values")
+
+            local function make_theme()
+                local theme = values.theme
+                local palette = values.theme.palette
+                if theme.name == "rose-pine" then
+                    return {
+                        line = { fg = palette.text, bg = palette.surface },
+                        head = { fg = palette.surface, bg = palette.iris, style = "italic" },
+                        current_tab = { fg = palette.surface, bg = palette.rose, style = "bold" },
+                        tab = { fg = palette.text, bg = palette.overlay, style = "bold" },
+                        win = { fg = palette.text, bg = palette.overlay },
+                        tail = { fg = palette.surface, bg = palette.rose, style = "bold" },
+                    }
+                end
+            end
+
+            local theme = make_theme()
+            vim.opt.showtabline = 2
+            require("tabby.tabline").set(function(line)
+                local cwd = " " .. vim.fn.fnamemodify(vim.fn.getcwd(), ":t") .. " "
+                return {
+                    { { cwd, hl = theme.head }, line.sep("", theme.head, theme.line) },
+                    line.tabs().foreach(function(tab)
+                        local hl = tab.is_current() and theme.current_tab or theme.tab
+                        return {
+                            line.sep("", hl, theme.line),
+                            tab.is_current() and "󰆤" or "󰆣",
+                            string.format("%s:", tab.number()),
+                            tab.name(),
+                            line.sep("", hl, theme.line),
+                            margin = " ",
+                            hl = hl,
+                        }
+                    end),
+                    line.spacer(),
+                    line.wins_in_tab(line.api.get_current_tab()).foreach(function(win)
+                        return {
+                            line.sep("", theme.win, theme.line),
+                            win.is_current() and "" or "",
+                            win.buf_name(),
+                            line.sep("", theme.win, theme.line),
+                            margin = " ",
+                            hl = theme.win,
+                        }
+                    end),
+                    { line.sep("", theme.tail, theme.line), { "  ", hl = theme.tail } },
+                    hl = theme.line,
+                }
+            end, {})
+        end,
         keys = {
-            { "<Tab>", "<CMD>BufferLineCycleNext<CR>", desc = "Next tab" },
-            { "<S-Tab>", "<CMD>BufferLineCyclePrev<CR>", desc = "Prev tab" },
+            { "<Tab>", "<CMD>tabnext<CR>", desc = "Next tab" },
+            { "<S-Tab>", "<CMD>tabprev<CR>", desc = "Prev tab" },
 
-            { "<leader>to", "<CMD>BufferLineCloseOthers<CR>", desc = "Close other tabs" },
-            { "<leader>tx", "<CMD>BufferLinePickClose<CR>", desc = "Pick tab to close" },
-            { "<leader>tp", "<CMD>BufferLinePick<CR>", desc = "Pick tab" },
+            { "<leader>tc", "<CMD>$tabnew<CR>", desc = "Create new tab" },
+            { "<leader>to", "<CMD>tabonly<CR>", desc = "Close other tabs" },
+            { "<leader>tx", "<CMD>tabclose<CR>", desc = "Pick tab to close" },
 
-            { "<leader>tmn", "<CMD>BufferLineMoveNext<CR>", desc = "Move current tab to next" },
-            { "<leader>tmp", "<CMD>BufferLineMovePrev<CR>", desc = "Move current tab to prev" },
-
-            -- vim.keymap.set("n", "<leader>tc", ":$tabnew<CR>", { desc = "Create new tab" })
-            -- vim.keymap.set("n", "<leader>tx", ":tabclose<CR>", { desc = "Close current tab" })
-            -- vim.keymap.set("n", "<leader>to", ":tabonly<CR>", { desc = "Close other tabs" })
-            -- vim.keymap.set("n", "<leader>tmp", ":-tabmove<CR>", { desc = "Move current tab to previous position" })
-            -- vim.keymap.set("n", "<leader>tmn", ":+tabmove<CR>", { desc = "Move current tab to next position " })
-        },
-        opts = {
-            options = {
-                mode = true,
-                show_buffer_close_icon = false,
-                show_close_icon = false,
-            },
+            { "<leader>tmn", "<CMD>+tabmove<CR>", desc = "Move current tab to next" },
+            { "<leader>tmp", "<CMD>-tabmove<CR>", desc = "Move current tab to prev" },
         },
     },
 }
