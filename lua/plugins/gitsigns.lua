9306e8838dd08f47842c904ab4955f8c3d865938
pcrab <pcrab@pcrab.xyz>
2025-03-30 20:10:25 +0800

    feat: git

---
+++ lua/plugins/gitsigns.lua
@@ -0,0 +1,11 @@
+return {
+    {
+        'lewis6991/gitsigns.nvim',
+        opts = {
+            current_line_blame = true,
+            current_line_blame_opts = {
+                delay = 0,
+            },
+        },
+    },
+}
