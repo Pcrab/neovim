c80960b01a87a9f87b35243709585bfe10fadea3
pcrab <pcrab@pcrab.xyz>
2025-04-01 23:16:34 +0800

    feat: use copilot.lua

--- lua/plugins/github-copilot.lua
+++ lua/plugins/github-copilot.lua
@@ -2,7 +2,7 @@ return {
     {
         'CopilotC-Nvim/CopilotChat.nvim',
         dependencies = {
-            { 'github/copilot.vim' },
+            { 'zbirenbaum/copilot.lua', cmd = 'Copilot', event = 'InsertEnter', opts = {} },
             { 'nvim-lua/plenary.nvim' },
         },
         build = 'make tiktoken', -- Only on MacOS or Linux
