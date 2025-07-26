057ac8cc0e495acad77bec1e8d5ee74ac559246b
pcrab <pcrab@pcrab.xyz>
2025-07-26 21:44:54 +0800

    feat: dap and ai

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -1,14 +1,17 @@
 vim.pack.add({
-	{ src = "https://github.com/mason-org/mason.nvim" },
-	{ src = "https://github.com/mason-org/mason-lspconfig.nvim" },
+
+	-- Common Dependencies
+	{ src = "https://github.com/nvim-lua/plenary.nvim" },
 	{ src = "https://github.com/neovim/nvim-lspconfig" },
 	{ src = "https://github.com/nvim-treesitter/nvim-treesitter" },
+	{ src = "https://github.com/nvim-tree/nvim-web-devicons" },
+
+	{ src = "https://github.com/mason-org/mason.nvim" },
+	{ src = "https://github.com/mason-org/mason-lspconfig.nvim" },
 
 	{ src = "https://github.com/folke/lazydev.nvim" },
 
 	{ src = "https://github.com/RRethy/vim-illuminate" },
-
-	{ src = "https://github.com/nvim-tree/nvim-web-devicons" },
 
 	-- LEAP repeat.vim for dot-repeats (.) to work
 	{ src = "https://github.com/ggandor/leap.nvim" },
@@ -23,9 +26,7 @@ vim.pack.add({
 	-- blink.download prebuilt binaries from gh release
 	{ src = "https://github.com/saghen/blink.pairs", version = "v0.3.0" },
 	{ src = "https://github.com/saghen/blink.download" },
-
 	{ src = "https://github.com/saghen/blink.indent" },
-
 	{
 		src = "https://github.com/Saghen/blink.cmp",
 		version = vim.version.range("1"),
@@ -34,5 +35,7 @@ vim.pack.add({
 	{ src = "https://github.com/ibhagwan/fzf-lua" },
 
 	{ src = "https://github.com/folke/todo-comments.nvim" },
-	{ src = "https://github.com/nvim-lua/plenary.nvim" },
+
+	{ src = "https://github.com/olimorris/codecompanion.nvim" },
+	{ src = "https://github.com/zbirenbaum/copilot.lua" },
 })
--- lua/core/setup.lua
+++ lua/core/setup.lua
@@ -114,3 +114,20 @@ require("blink.indent").setup({
 })
 
 require("todo-comments").setup({})
+
+require("copilot").setup({
+	suggestion = {
+		auto_trigger = true,
+		keymap = {
+			accept = false,
+		},
+	},
+})
+vim.keymap.set("i", "<Tab>", function()
+	if require("copilot.suggestion").is_visible() then
+		require("copilot.suggestion").accept()
+	else
+		vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Tab>", true, false, true), "n", false)
+	end
+end, { desc = "Super Tab" })
+require("codecompanion").setup({})
